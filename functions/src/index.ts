8a09cae854e5cf67c651431b091a71af4059414e
Sohn Young Jin <sonkim1001@naver.com>
2023-04-07 01:45:38 +0900

    feat: 새로운 사용자를 만드는 기능 추가

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const firestore = admin.firestore();
+const firestoreUserRef = firestore.collection("users");
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+});
+
+export const createUser = functions.https.onCall(async (data, context) => {
+    try {
+        const userId = data.user_id
+        const user = data.user;
+        const newUserRef = firestoreUserRef.doc(userId);
+        await newUserRef.set(user);
+        return { success: true, userId: newUserRef.id };
+    } catch (error) {
+        return { success: false, error };
+    }
+});
+

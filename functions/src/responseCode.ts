cd7305ef2e7329c971acc7343dace82a5a48a893
Sohn Young Jin <sonkim1001@naver.com>
2023-04-07 22:44:46 +0900

    feat: 서버 응답 코드를 모아놓은 responseCodes.ts 추가

---
+++ functions/src/responseCode.ts
@@ -0,0 +1,17 @@
+export const ResponseCodes = {
+    INVALID_ID_TOKEN: "auth/invalid-id-token",
+    UNKNOWN_ERROR: "functions/unknown-error",
+
+    USER_NOT_FOUND: "user/user-not-found",
+    NICKNAME_NOT_SET: "user/nickname-not-set",
+    NICKNAME_ALREADY_IN_USE: "user/nickname-already-in-use",
+    DETAIL_INFO_NOT_SET: "user/detail-info-not-set",
+
+    TRIP_NOT_FOUND: "trip/trip-not-found",
+    
+    EVENT_NOT_FOUND: "event/event-not-found",
+
+    POST_NOT_FOUND: "post/post-not-found",
+
+    COMMENT_NOT_FOUND: "comment/comment-not-found",
+};

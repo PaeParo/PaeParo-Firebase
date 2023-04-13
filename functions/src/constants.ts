c72fc6ef0235786a9299925a52bc00f2c15236e7
Sohn Young Jin <sonkim1001@naver.com>
2023-04-13 17:24:09 +0900

    feat: Create constants.ts for save constant values

---
+++ functions/src/constants.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export enum TripUpdateType {
+    NONE,
+    CREATE,
+    ADD,
+    REMOVE,
+    UPDATE
+}
+
+export enum EventType {
+    NONE,
+    PLACE,
+    MOVE,
+    MEAL
+}

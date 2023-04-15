d9e549f3cf953f78eff2e5a950c4961d50c9a254
Sohn Young Jin <sonkim1001@naver.com>
2023-04-16 01:19:00 +0900

    feat: 게시물 및 댓글 관련 Functions 함수 구현
    -게시물 관련:  createPost, updatePost, deletePost
    - 댓글 관련: addComment, updateComment, deleteComment

--- functions/src/commentFunctions.ts
+++ functions/src/commentFunctions.ts
@@ -1,21 +1,72 @@
 /* eslint-disable */
-import { firestore, functions, firestoreCommentRef } from "./firebase";
+import { firestore, functions, firestorePostRef, firestoreCommentRef } from "./firebase";
 import { ResponseCodes } from "./responseCode";
+
+/**
+ * 5-01. 특정 게시물과 연결된 댓글 추가
+ */
+export const addComment = functions.https.onCall(async (data) => {
+    try {
+        const comment = data.comment;
+        const commentRef = firestoreCommentRef.doc();
+        const postRef = firestorePostRef.doc(comment.post_id);
+
+        if (!(await postRef.get()).exists) {
+            console.log("[commentFunctions/addComment] Post not found: " + comment.post_id);
+            return { result: ResponseCodes.FAILURE, type: ResponseCodes.POST_NOT_FOUND };
+        }
+
+        await commentRef.set(comment);
+
+        console.log("[commentFunctions/addComment] Comment created: " + commentRef.id);
+        return { result: ResponseCodes.SUCCESS, data: commentRef.id };
+    } catch (error) {
+        console.log("[commentFunctions/addComment] Error: " + error);
+        return { result: ResponseCodes.FAILURE, type: ResponseCodes.SERVER_ERROR };
+    }
+});
 
 /**
  * 5-02. 특정 댓글 수정
  */
-export const updateComment = functions.https.onCall(async (data, context) => {
+export const updateComment = functions.https.onCall(async (data) => {
     try {
+        const commentId = data.comment_id;
+        const updateFields = data.update_fields;
+
+        if (!(await firestoreCommentRef.doc(commentId).get()).exists) {
+            console.log("[commentFunctions/updateComment] Comment not found: " + commentId);
+            return { result: ResponseCodes.FAILURE, type: ResponseCodes.COMMENT_NOT_FOUND };
+        }
+
+        await firestoreCommentRef.doc(commentId).update(updateFields);
+
+        console.log("[commentFunctions/updateComment] Comment updated: " + commentId);
+        return { result: ResponseCodes.SUCCESS };
     } catch (error) {
+        console.log("[commentFunctions/updateComment] Error: " + error);
+        return { result: ResponseCodes.FAILURE, type: ResponseCodes.SERVER_ERROR };
     }
 });
 
 /**
  * 5-03. 특정 댓글 삭제
  */
-export const deleteComment = functions.https.onCall(async (data, context) => {
+export const deleteComment = functions.https.onCall(async (data) => {
     try {
+        const commentId = data.comment_id;
+
+        if (!(await firestoreCommentRef.doc(commentId).get()).exists) {
+            console.log("[commentFunctions/deleteComment] Comment not found: " + commentId);
+            return { result: ResponseCodes.FAILURE, type: ResponseCodes.COMMENT_NOT_FOUND };
+        }
+
+        await firestoreCommentRef.doc(commentId).delete();
+
+        console.log("[commentFunctions/deleteComment] Comment deleted: " + commentId);
+        return { result: ResponseCodes.SUCCESS };
     } catch (error) {
+        console.log("[commentFunctions/deleteComment] Error: " + error);
+        return { result: ResponseCodes.FAILURE, type: ResponseCodes.SERVER_ERROR };
     }
 });
--- functions/src/postFunctions.ts
+++ functions/src/postFunctions.ts
@@ -1,30 +1,72 @@
 /* eslint-disable */
-import { firestore, functions, firestorePostRef } from "./firebase";
+import { firestore, functions, firestorePostRef, firestoreTripRef } from "./firebase";
 import { ResponseCodes } from "./responseCode";
 
 /**
  * 4-01. 특정 여행과 연결된 게시물 추가
  */
-export const createPost = functions.https.onCall(async (data, context) => {
+export const createPost = functions.https.onCall(async (data) => {
     try {
+        const post = data.post;
+        const postRef = firestorePostRef.doc();
+        const tripRef = firestoreTripRef.doc(post.trip_id);
+
+        if (!(await tripRef.get()).exists) {
+            console.log("[postFunctions/createPost] Trip not found: " + post.trip_id);
+            return { result: ResponseCodes.FAILURE, type: ResponseCodes.TRIP_NOT_FOUND };
+        }
+
+        await postRef.set(post);
+
+        console.log("[postFunctions/createPost] Post created: " + postRef.id);
+        return { result: ResponseCodes.SUCCESS, data: postRef.id };
     } catch (error) {
+        console.log("[postFunctions/createPost] Error: " + error);
+        return { result: ResponseCodes.FAILURE, type: ResponseCodes.SERVER_ERROR };
     }
 });
 
 /**
  * 4-04. 특정 게시물 수정
  */
-export const updatePost = functions.https.onCall(async (data, context) => {
+export const updatePost = functions.https.onCall(async (data) => {
     try {
+        const postId = data.post_id;
+        const updateFields = data.update_fields;
+
+        if (!(await firestorePostRef.doc(postId).get()).exists) {
+            console.log("[postFunctions/updatePost] Post not found: " + postId);
+            return { result: ResponseCodes.FAILURE, type: ResponseCodes.POST_NOT_FOUND };
+        }
+
+        await firestorePostRef.doc(postId).update(updateFields);
+
+        console.log("[postFunctions/updatePost] Post updated: " + postId);
+        return { result: ResponseCodes.SUCCESS };
     } catch (error) {
+        console.log("[postFunctions/updatePost] Error: " + error);
+        return { result: ResponseCodes.FAILURE, type: ResponseCodes.SERVER_ERROR };
     }
 });
 
 /**
  * 4-05. 특정 게시물 삭제
  */
-export const deletePost = functions.https.onCall(async (data, context) => {
+export const deletePost = functions.https.onCall(async (data) => {
     try {
+        const postId = data.post_id;
+
+        if (!(await firestorePostRef.doc(postId).get()).exists) {
+            console.log("[postFunctions/deletePost] Post not found: " + postId);
+            return { result: ResponseCodes.FAILURE, type: ResponseCodes.POST_NOT_FOUND };
+        }
+
+        await firestorePostRef.doc(postId).delete();
+
+        console.log("[postFunctions/deletePost] Post deleted: " + postId);
+        return { result: ResponseCodes.SUCCESS };
     } catch (error) {
+        console.log("[postFunctions/deletePost] Error: " + error);
+        return { result: ResponseCodes.FAILURE, type: ResponseCodes.SERVER_ERROR };
     }
 });

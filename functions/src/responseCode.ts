dd79023dfd9a70ed7f909b3a1867b6f73009103e
Sohn Young Jin <sonkim1001@naver.com>
2023-04-15 15:57:33 +0900

    feat: ResponseCode 추가 - SERVER_ERROR, CLIENT_ERROR, ALL_DATA_SET

--- functions/src/responseCode.ts
+++ functions/src/responseCode.ts
@@ -1,12 +1,14 @@
 /* eslint-disable */
 export const ResponseCodes = {
     INVALID_ID_TOKEN: "auth/invalid-id-token",
-    UNKNOWN_ERROR: "functions/unknown-error",
 
     SUCCESS: "success",
     FAILURE: "failure",
-    
+    SERVER_ERROR: "server-error",
+    CLIENT_ERROR: "client-error",
+
     USER_NOT_FOUND: "user/user-not-found",
+    ALL_DATA_SET: "user/all-data-set",
     NICKNAME_NOT_SET: "user/nickname-not-set",
     NICKNAME_ALREADY_IN_USE: "user/nickname-already-in-use",
     DETAIL_INFO_NOT_SET: "user/detail-info-not-set",
